ea2813ec15f2b3ee2cc297f2e92b88f98f10ecd3
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-08-09 07:54:11 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+
+import API, { graphqlOperation } from "@aws-amplify/api";
+
+import { createTodo } from "./graphql/mutations";
+import { onCreateTodo } from "./graphql/subscriptions";
+import { OnCreateTodoSubscription, Todo, CreateTodoInput } from "./API";
+
+import "./App.css";
+
+const QUERY = "QUERY";
+const SUBSCRIPTION = "SUBSCRIPTION";
+
+type StateType = {
+  todos: Todo[];
+};
+
+type ActionType =
+  | {
+      type: "SUBSCRIPTION";
+      todo: Todo;
+    }
+  | { type: "QUERY"; todos: Todo[] };
+
+type SubscriptionValue = {
+  value: {
+    data: OnCreateTodoSubscription;
+  };
+};
+
+const initialState: StateType = {
+  todos: [],
+};
+
+const reducer = (state: StateType, action: ActionType) => {
+  switch (action.type) {
+    case QUERY:
+      return { ...state, todos: action.todos };
+    case SUBSCRIPTION:
+      return { ...state, todos: [...state.todos, action.todo] };
+    default:
+      return state;
+  }
+};
+
+async function createNewTodo() {
+  const todo: CreateTodoInput = {
+    name: "Todo " + Math.floor(Math.random() * 10),
+  };
+  await API.graphql(graphqlOperation(createTodo, { input: todo }));
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    // @ts-ignore (https://github.com/aws-amplify/amplify-js/issues/7589)
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+      next: ({ value }: SubscriptionValue) => {
+        const todo = value.data.onCreateTodo;
+        if (todo) {
+          dispatch({ type: SUBSCRIPTION, todo });
+        }
+      },
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createNewTodo}>Add Todo</button>
+      <div>
+        {state.todos.length > 0 ? (
+          state.todos.map((todo: Todo) => (
+            <p key={todo.id}>
+              {todo.name} ({todo.createdAt})
+            </p>
+          ))
+        ) : (
+          <p>Add some todos!</p>
+        )}
+      </div>
     </div>
   );
 }
